217ea4f27f0f4d7b19a8a2fc514159eaae74bb27
AmirastanM <amirastanshm@code.edu.az>
2024-03-16 15:55:51 +0400

    control save

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using Service.Services.Helpers.Extensions;
+
+GetMenues();
+
+while (true)
+{
+
+
+}
+
+
+
+
+
+
+
+void GetMenues()
+{
+    ConsoleColor.Cyan.WriteConsole("Please choose operation : \n 1 - GroupCreate,\r\n 2 - GroupEdit,\r\n 3 - GroupDelete,\r\n" +
+        " 4 - GetGroupById,\r\n 5 -  GetAllGroupsByTeacher,\r\n 6 - GetAllGroupsByRoom,\r\n 7 - GetAllGroups,\r\n" +
+        " 8 - SearchGroupsByName1 - Create Group\n   ");
+}
